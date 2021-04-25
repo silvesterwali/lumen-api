d766afbb6a85a8e708e38b7e2537bb54dd1bfa56
silvesterwali <silvesterlhwali123@gmail.com>
2021-04-25 23:53:00 +0800

    add usersWithPermissions \Illuminate\Http\Controllers\UserPermissionController.php

--- routes/web.php
+++ routes/web.php
@@ -50,15 +50,16 @@ $router->group(["middleware" => "auth:api", "prefix" => "api"], function () use 
     });
 
     $router->group(["prefix" => "user_permission"], function () use ($router) {
+        $router->get("/", "UserPermissionController@usersWithPermissions");
         $router->get("/permission/{permission}", "UserPermissionController@usersWithPermission");
         $router->post("/give_permission_to", "UserPermissionController@userGivePermissionTo");
         $router->post("/revoke_permission_to", "UserPermissionController@userRevokePermissionTo");
     });
 
     $router->group(["prefix" => "user_role"], function () use ($router) {
+        $router->get("/", "UserRoleController@usersWithRoles");
         $router->get("/role/{role}", "UserRoleController@usersWithRole");
         $router->get("/user_without_role", "UserRoleController@usersWithoutRole");
-        $router->get("/user_with_role", "UserRoleController@usersWithRoles");
         $router->post("/assign_role", "UserRoleController@userAssignRole");
         $router->post("/remove_role_as", "UserRoleController@userRemoveRoleAs");
 
