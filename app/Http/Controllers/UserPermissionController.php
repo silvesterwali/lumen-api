69d20d77f319ffa00442fa12eb62a9cc27a5286d
silvesterwali <silvesterlhwali123@gmail.com>
2021-04-25 22:11:43 +0800

    rename \App\Http\Controllers\UserPermission.php to \App\Http\Controllers\UserPermissionController.php add \App\Http\Controllers\UserRolePermissionController.php

--- app/Http/Controllers/UserPermissionController.php
+++ app/Http/Controllers/UserPermissionController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 
-class UserPermission extends Controller
+class UserPermissionController extends Controller
 {
     /**
      * give permissions to a user
