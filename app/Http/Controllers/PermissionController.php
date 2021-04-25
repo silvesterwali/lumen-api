d8193f755e73268c06fc719ed6b66de7740f5f4e
silvesterwali <silvesterlhwali123@gmail.com>
2021-04-25 21:54:43 +0800

    * refacktor \App\Http\Controllers\PermissionController.php * create \App\Http\Controllers\UserPermission.php

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Permission;
-use Symfony\Component\HttpFoundation\Response;
 
 class PermissionController extends Controller
 {
--- app/Http/Controllers/UserPermission.php
+++ app/Http/Controllers/UserPermission.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserPermission extends Controller
 {
-    //
+    public function UserGivePermissionTo(Request $request)
+    {
+        $this->validate($request, [
+            "user_id" => "required",
+            "permission" => "required",
+        ]);
+
+        $user = User::findOrFail($request->user_id);
+        $user->givePermissionTo($request->permission);
+    }
 }
