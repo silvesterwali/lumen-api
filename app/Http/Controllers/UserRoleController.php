21e9bf30276e49e6f316cf8dd03345bf1f2e55a4
silvesterwali <silvesterlhwali123@gmail.com>
2021-04-25 22:19:29 +0800

    add userAssignRole endpoint to \App\Http\Controllers\UserRoleController.php

--- app/Http/Controllers/UserRoleController.php
+++ app/Http/Controllers/UserRoleController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserRoleController extends Controller
 {
-    //
+    /**
+     * assign a role to user
+     * @param \Illuminate\Http\Request $request
+     * @param \Illuminate\Http\Response
+     */
+    public function userAssignRole(Request $request)
+    {
+        $this->validate($request, [
+            "user_id" => "required",
+            "role" => "required",
+        ]);
+
+        $user = User::findOrFail($request->user_id);
+
+        $user->assignRole($request->role);
+
+        return response(["message" => "Success to assign role to a user"]);
+
+    }
 }
