1614f55b8673df64a3b6269df64fe982c631f025
silvesterwali <silvesterlhwali123@gmail.com>
2021-04-26 00:25:34 +0800

    add \App\Http\Controllers\UserRolePermissionController.php

---
+++ app/Http/Controllers/UserRoleToPermissionController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class UserRoleToPermissionController extends Controller
+{
+    /**
+     * all user with specific role give permission to
+     * @param \Illuminate\Http\Request #$request
+     * @return \Illuminate\Http\Response
+     */
+    public function userRoleGivePermissionTo(Request $request)
+    {
+        $this->validate($request, [
+            "role" => "required",
+            "permission" => "required",
+        ]);
+
+        $users = User::permission($request->role)
+            ->pluck("id")
+            ->toArray();
+
+        if (!$users) {
+            return response(["message" => "User with given role not found"], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        foreach ($users as $userId) {
+
+            $currentUser = User::find($userId);
+            $currentUser->givePermissionTo($request->permission);
+        }
+
+        return response(["message" => "Success to add a permission to specific user with via role "]);
+    }
+
+    /**
+     * revoke a permission from user with specific role
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function userRoleRevokePermissionTo(Request $request)
+    {
+        $this->validate($request, [
+            "role" => "required",
+            "permission" => "required",
+        ]);
+
+        $users = User::permission($request->role)
+            ->pluck("id")
+            ->toArray();
+
+        if (!$users) {
+            return response(["message" => "User with given role not found"], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        foreach ($users as $userId) {
+
+            $currentUser = User::find($userId);
+            $currentUser->revokePermissionTo($request->permission);
+        }
+
+        return response(["message" => "Success to revoke a permission from user via role "]);
+    }
+}
