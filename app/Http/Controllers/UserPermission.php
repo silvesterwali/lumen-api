84c50da20d0d85d6a327c1a8d65b606b8b610362
silvesterwali <silvesterlhwali123@gmail.com>
2021-04-25 22:06:06 +0800

    add UserGivePermissionTo and UserRevokePermissionTo \App\Http\Controllers\UserPermission.php

--- app/Http/Controllers/UserPermission.php
+++ app/Http/Controllers/UserPermission.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class UserPermission extends Controller
 {
+    /**
+     * give permissions to a user
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
     public function UserGivePermissionTo(Request $request)
     {
         $this->validate($request, [
@@ -16,5 +21,26 @@ class UserPermission extends Controller
 
         $user = User::findOrFail($request->user_id);
         $user->givePermissionTo($request->permission);
+
+        return response(["message" => "Success add permission to user"]);
     }
+
+    /**
+     * revoke a permission from a user
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function UserRevokePermissionTo(Request $request)
+    {
+        $this->validate($request, [
+            "user_id" => "required",
+            "permission" => "required",
+        ]);
+
+        $user = User::findOrFail($$request->user_id);
+        $user->revokePermissionTo($request->permission);
+
+        return response(["success to revoke a permission from a ser"]);
+    }
+
 }
