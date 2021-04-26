c56d020001accf57db87771a36683c46d2caed35
silvesterwali <silvesterlhwali123@gmail.com>
2021-04-27 00:15:11 +0800

    add public relation table navigation drawer->child

--- app/Models/NavigationDrawer.php
+++ app/Models/NavigationDrawer.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class NavigationDrawer extends Model
 {
     protected $fillable = ["name", "path_name", "icon", "description", "level"];
+
+    public function navigation_drawer_child()
+    {
+        return $this->hasMany(NavigationDrawerChild::class, "navigation_drawer_id");
+    }
 }
--- app/Models/NavigationDrawerChild.php
+++ app/Models/NavigationDrawerChild.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class NavigationDrawerChild extends Model
 {
-    //
+    protected $fillable = ["name", "navigation_drawer_id", "path_name", "icon", "level", "description"];
+
+    public function navigation_drawer()
+    {
+        return $this->belongsTo(NavigationDrawer::class, "id");
+    }
 }
