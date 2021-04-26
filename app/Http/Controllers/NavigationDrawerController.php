3542f0842aa2608e3227057e1286174071f94476
silvesterwali <silvesterlhwali123@gmail.com>
2021-04-26 18:24:38 +0800

    add navigation_drawer migrations

---
+++ app/Http/Controllers/NavigationDrawerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NavigationDrawer;
+use Illuminate\Http\Request;
+
+class NavigationDrawerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\NavigationDrawer  $navigationDrawer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(NavigationDrawer $navigationDrawer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\NavigationDrawer  $navigationDrawer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(NavigationDrawer $navigationDrawer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\NavigationDrawer  $navigationDrawer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, NavigationDrawer $navigationDrawer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\NavigationDrawer  $navigationDrawer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(NavigationDrawer $navigationDrawer)
+    {
+        //
+    }
+}
