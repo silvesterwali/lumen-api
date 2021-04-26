38ad8f90da4a41cb679783ed16b269e7b0966724
silvesterwali <silvesterlhwali123@gmail.com>
2021-04-26 19:20:54 +0800

    add logic to \App\Http\Controllers\NavigationDrawerController/php

--- app/Http/Controllers/NavigationDrawerController.php
+++ app/Http/Controllers/NavigationDrawerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\NavigationDrawer;
 use Illuminate\Http\Request;
+use Services\NavigationDrawerService;
 
 class NavigationDrawerController extends Controller
 {
@@ -25,7 +26,7 @@ class NavigationDrawerController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, NavigationDrawerService $navService)
     {
         $this->validate($request, [
             "name" => "required|unique:navigation_drawers,name",
@@ -33,6 +34,11 @@ class NavigationDrawerController extends Controller
             "icon" => "string",
             "description" => "string",
         ]);
+
+        $request->level = $navService->lastMaxLevel();
+        NavigationDrawer::create($request);
+        return response(["message" => "Success to add new navigation drawer"]);
+
     }
 
     /**
@@ -43,30 +49,29 @@ class NavigationDrawerController extends Controller
      */
     public function show(NavigationDrawer $navigationDrawer)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\NavigationDrawer  $navigationDrawer
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(NavigationDrawer $navigationDrawer)
-    {
-        //
+        return response($navigationDrawer);
     }
 
     /**
      * Update the specified resource in storage.
-     *
+     * note : level cannot update here
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\NavigationDrawer  $navigationDrawer
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, NavigationDrawer $navigationDrawer)
     {
-        //
+        $this->validate($request, [
+            "name" => "required|unique:navigation_drawers,name," . $navigationDrawer->id,
+            "path_name" => "required|unique:navigation_drawers,path_name," . $navigationDrawer->id,
+            "level" => "required|unique:level,path_name," . $navigationDrawer->id,
+            "icon" => "string",
+            "description" => "string",
+        ]);
+
+        $navigationDrawer->update($request->except("id"));
+        return response(["message" => "Success to update navigation drawer"]);
+
     }
 
     /**
@@ -77,6 +82,7 @@ class NavigationDrawerController extends Controller
      */
     public function destroy(NavigationDrawer $navigationDrawer)
     {
-        //
+        $navigationDrawer->delete();
+        return response(["message" => "Success to delete navigation drawer"]);
     }
 }
