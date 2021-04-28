464c9e0e2b85ef5d77ea039d8e7571cacc9cf5e2
silvesterwali <silvesterlhwali123@gmail.com>
2021-04-29 00:50:41 +0800

    * add logic to method on \App\Http\Controllers\NavigationAccordingRoleController.php

--- app/Http/Controllers/NavigationAccordingRoleController.php
+++ app/Http/Controllers/NavigationAccordingRoleController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\UserNavigationItem;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use \Spatie\Permission\Models\Role;
 
 class NavigationAccordingRoleController extends Controller
 {
@@ -13,7 +17,40 @@ class NavigationAccordingRoleController extends Controller
      */
     public function giveNavigationToRole(Request $request)
     {
+        $this->validate($request, [
+            "role"                       => "required|string",
+            "navigation_drawer_child_id" => "required|integer",
+        ]);
 
+        $role = Role::findByName($request->role);
+
+        if (!$role) {
+
+            return response(["message" => "The role you are aiming for does not exist"], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $users = User::role($request->role)->get();
+
+        if (!$users) {
+            return response(["message" => "There is no user data"], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $users->map(function ($user) use ($request) {
+
+            UserNavigationItem::updateOrCreate(
+                [
+                    "user_id"                    => $user->id,
+                    "navigation_drawer_child_id" => $request->navigation_drawer_child_id,
+                ],
+                [
+                    "user_id"                    => $user->id,
+                    "navigation_drawer_child_id" => $request->navigation_drawer_child_id,
+                ]
+            );
+
+        });
+
+        return response(["message" => "Success provides navigation for users via role"]);
     }
 
     /**
@@ -23,6 +60,31 @@ class NavigationAccordingRoleController extends Controller
      */
     public function revokeNavigationFromRole(Request $request)
     {
+        $this->validate($request, [
+            "role"                       => "required|string",
+            "navigation_drawer_child_id" => "required|integer",
+        ]);
 
+        $role = Role::findByName($request->role);
+
+        if (!$role) {
+
+            return response(["message" => "The role you are aiming for does not exist"], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $users = User::role($request->role)->get();
+
+        if (!$users) {
+            return response(["message" => "There is no user data"], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $users->map(function ($user) use ($request) {
+
+            UserNavigationItem::where("user_id", $user->user_id)
+                ->where("navigation_drawer_child_id", $request->navigation_drawer_child_id)
+                ->delete();
+        });
+
+        return response(["message" => "Success provides navigation for users via role"]);
     }
 }
