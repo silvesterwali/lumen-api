cadb668e347bb9896f6d32799fe679308c65d1fd
silvesterwali <silvesterlhwali123@gmail.com>
2021-04-29 00:18:22 +0800

    * add \App\Http\Controllers\NavigationAccordingRoleController.php * add giveNavigationToRole & revokeNavigationFromRole on \App\Http\Controllers\NavigationAccordingRoleContoller.php

---
+++ app/Http/Controllers/NavigationAccordingRoleController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NavigationAccordingRoleController extends Controller
+{
+    /**
+     * give a navigation to user's with a role
+     * @param \Illuminate\Http\Request
+     * @return \Illuminate\Http\Response
+     */
+    public function giveNavigationToRole(Request $request)
+    {
+
+    }
+
+    /**
+     * revoke a navigation from user's with a role
+     * @param \Illuminate\Http\Request;
+     * @param \Illuminate\Http\Response
+     */
+    public function revokeNavigationFromRole(Request $request)
+    {
+
+    }
+}
