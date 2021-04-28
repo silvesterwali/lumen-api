99235abbc70a077e4e295b1928c2c8c57f3f5f9c
silvesterwali <silvesterlhwali123@gmail.com>
2021-04-28 23:51:41 +0800

    fix User Navigation drawer

--- app/Models/UserNavigationItem.php
+++ app/Models/UserNavigationItem.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserNavigationItem extends Model
 {
+    protected $fillable = ["user_id", "navigation_drawer_child_id"];
     public function users()
     {
         return $this->hasMany(User::class, "user_id");
