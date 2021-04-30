c5b51da76216b3981e8d40b87862400439cbfd85
silvesterwali <silvesterlhwali123@gmail.com>
2021-05-01 00:51:33 +0800

    refaktor /routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -17,61 +17,80 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-// route for basic authentication
+/**
+ * the route to perform basic authentication on the application
+ */
 $router->group(['prefix' => 'api'], function () use ($router) {
     $router->post('register', 'AuthController@register');
     $router->post('login', 'AuthController@login');
 });
 
-// all route with authorize
-
+/**
+ * all route with authorize
+ */
 $router->group(["middleware" => "auth:api", "prefix" => "api"], function () use ($router) {
 
-    // route for user basic
+    /**
+     * specific route for user ownership
+     */
     $router->group(["prefix" => "auth"], function () use ($router) {
         $router->post("logout", "AuthController@logout");
         $router->get("refresh", "AuthController@refresh");
         $router->get("me", "AuthController@me");
     });
 
-    // resource role
+    /**
+     * resource routes for user role
+     */
     $router->group(["prefix" => "role"], function () use ($router) {
         $router->get("/", "RoleController@index");
         $router->post("/", "RoleController@store");
         $router->delete("/{id}", "RoleController@destroy");
     });
 
-    // resource permission
+    /**
+     * user permission resource route
+     */
     $router->group(["prefix" => "permission"], function () use ($router) {
         $router->get("/", "PermissionController@index");
         $router->post("/", "PermissionController@store");
         $router->delete("/{id}", "PermissionController@destroy");
     });
 
-    $router->group(["prefix" => "user_permission"], function () use ($router) {
+    /**
+     * the route of the relationship between users and roles
+     */
+    $router->group(["prefix" => "user-permission"], function () use ($router) {
         $router->get("/", "UserPermissionController@usersWithPermissions");
         $router->get("/permission/{permission}", "UserPermissionController@usersWithPermission");
-        $router->post("/give_permission", "UserPermissionController@userGivePermissionTo");
-        $router->post("/revoke_permission", "UserPermissionController@userRevokePermissionTo");
+        $router->post("/give-permission", "UserPermissionController@userGivePermissionTo");
+        $router->post("/revoke-permission", "UserPermissionController@userRevokePermissionTo");
     });
 
-    $router->group(["prefix" => "user_role"], function () use ($router) {
+    /**
+     * the route of the relationship between users and roles
+     */
+    $router->group(["prefix" => "user-role"], function () use ($router) {
         $router->get("/", "UserRoleController@usersWithRoles");
         $router->get("/role/{role}", "UserRoleController@usersWithRole");
-        $router->get("/user_without_role", "UserRoleController@usersWithoutRole");
-        $router->post("/assign_role", "UserRoleController@userAssignRole");
-        $router->post("/remove_role_as", "UserRoleController@userRemoveRoleAs");
+        $router->get("/user-without-role", "UserRoleController@usersWithoutRole");
+        $router->post("/give-role", "UserRoleController@userAssignRole");
+        $router->post("/revoke-role", "UserRoleController@userRemoveRoleAs");
 
     });
 
-    //  give and revoke a user's permission
-    $router->group(["prefix" => "user_via_role"], function () use ($router) {
-        $router->post("/give_permission", "UserRoleToPermissionController@userRoleGivePermissionTo");
-        $router->post("/revoke_permission", "UserRoleToPermissionController@userRoleRevokePermissionTo");
+    /**
+     * give and revoke a user's permission via role
+     */
+    $router->group(["prefix" => "user-via-role"], function () use ($router) {
+        $router->post("/give-permission", "UserRoleToPermissionController@userRoleGivePermissionTo");
+        $router->post("/revoke-permission", "UserRoleToPermissionController@userRoleRevokePermissionTo");
     });
 
-    // resource navigation drawer
-    $router->group(["prefix" => "navigation_drawer"], function () use ($router) {
+    /**
+     * resource navigation drawer
+     */
+    $router->group(["prefix" => "navigation-drawer"], function () use ($router) {
         $router->get("/", "NavigationDrawerController@index");
         $router->post("/", "NavigationDrawerController@store");
         $router->get("/{id}", "NavigationDrawerController@show");
@@ -79,8 +98,10 @@ $router->group(["middleware" => "auth:api", "prefix" => "api"], function () use 
         $router->delete("/{id}", "NavigationDrawerController@destroy");
     });
 
-    // resource navigation drawer child
-    $router->group(["prefix" => "navigation_drawer_child"], function () use ($router) {
+    /**
+     * resource navigation drawer child
+     */
+    $router->group(["prefix" => "navigation-drawer-child"], function () use ($router) {
         $router->get("/", "NavigationDrawerChildController@index");
         $router->post("/", "NavigationDrawerChildController@store");
         $router->get("/{id}", "NavigationDrawerChildController@show");
@@ -88,19 +109,25 @@ $router->group(["middleware" => "auth:api", "prefix" => "api"], function () use 
         $router->delete("/{id}", "NavigationDrawerChildController@destroy");
     });
 
-    // all navigation assigned to a user
+    /**
+     *  all navigation assigned to a user
+     */
     $router->get("navigation_items/{user_id}/user", "UserNavigationItemController");
 
-    // give and revoke a user's navigation
-    $router->group(["prefix" => "user_navigation"], function () use ($router) {
-        $router->post("/give_navigation", "UserNavigationController@giveNavigationToUser");
-        $router->post("/revoke_navigation", "UserNavigationController@revokeNavigationFromUser");
+    /**
+     * give and revoke a user's navigation
+     */
+    $router->group(["prefix" => "user-navigation"], function () use ($router) {
+        $router->post("/give-navigation", "UserNavigationController@giveNavigationToUser");
+        $router->post("/revoke-navigation", "UserNavigationController@revokeNavigationFromUser");
     });
 
-    // give and revoke navigation to role
-    $router->group(["prefix" => "role_navigation"], function () use ($router) {
-        $router->post("/give_navigation", "NavigationAccordingRoleController@giveNavigationToRole");
-        $router->post("/revoke_navigation", "NavigationAccordingRoleController@revokeNavigationFromRole");
+    /**
+     * give and revoke navigation to role
+     */
+    $router->group(["prefix" => "role-navigation"], function () use ($router) {
+        $router->post("/give-navigation", "NavigationAccordingRoleController@giveNavigationToRole");
+        $router->post("/revoke-navigation", "NavigationAccordingRoleController@revokeNavigationFromRole");
     });
 
 });
