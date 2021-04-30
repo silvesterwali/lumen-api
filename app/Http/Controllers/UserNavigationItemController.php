a5b25d085b8bed0473b957b01cac612f8b69922d
silvesterwali <silvesterlhwali123@gmail.com>
2021-05-01 00:08:03 +0800

    fix navigation user

--- app/Http/Controllers/UserNavigationItemController.php
+++ app/Http/Controllers/UserNavigationItemController.php
@@ -13,14 +13,33 @@ class UserNavigationItemController extends Controller
      */
     public function __invoke($user_id)
     {
-        $userNavItem = NavigationDrawer::whereHas('navigation_drawer_child', function ($q) use ($user_id) {
-            $q->whereHas(
-                'userNavigationItems', function ($q2) use ($user_id) {
-                    $q2->where('user_id', $user_id);
-                },
+        $userNavItem = NavigationDrawer::with(
+            ["navigation_drawer_child" => function ($q) use ($user_id) {
+                // get relationship table with condition
+                $q->whereHas("userNavigationItems", function ($qq) use ($user_id) {
 
-            );
-        })->with('navigation_drawer_child')->get();
+                    $qq->where("user_id", $user_id)
+
+                        ->orderBy("level", "asc");
+
+                });
+            },
+            ]
+        )
+
+            ->whereHas("navigation_drawer_child", function ($q) use ($user_id) {
+                // if current relationship is empty whereHas will reduce to prevent return data
+                // without any data on relationship eager loading
+                $q->whereHas("userNavigationItems", function ($qq) use ($user_id) {
+
+                    $qq->where("user_id", $user_id)
+
+                        ->orderBy("level", "asc");
+
+                });
+            })
+            ->orderBy('level', "asc")
+            ->get();
 
         return response($userNavItem);
     }
