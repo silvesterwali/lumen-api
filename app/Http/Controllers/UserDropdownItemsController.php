038a676f6128428939001b05440add81403af153
silvesterwali <silvesterlhwali123@gmail.com>
2021-05-04 23:56:42 +0800

    add __invoke method to \App\Http\Contollers\UserDropdownItemControlller.php

--- app/Http/Controllers/UserDropdownItemsController.php
+++ app/Http/Controllers/UserDropdownItemsController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\NavigationDropdown;
 
 class UserDropdownItemsController extends Controller
 {
-    //
+    /**
+     * @param int $user_id
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke($user_id)
+    {
+        $navigationDropdown = NavigationDropdown::whereHas("userDropdown", function ($query) use ($user_id) {
+            $query->where("user_id", $user_id);
+        })->orderBy("level", "asc")
+            ->get();
+
+        return response($navigationDropdown);
+    }
 }
