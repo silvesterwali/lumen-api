f21e96e5f99552c04ed68add84ac0d5df8675f9b
silvesterwali <silvesterlhwali123@gmail.com>
2021-05-04 22:51:13 +0800

    add user_dropdown menu

---
+++ app/Http/Controllers/UserDropdownController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserDropdown;
+use Illuminate\Http\Request;
+
+class UserDropdownController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\UserDropdown  $userDropdown
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserDropdown $userDropdown)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\UserDropdown  $userDropdown
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserDropdown $userDropdown)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\UserDropdown  $userDropdown
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserDropdown $userDropdown)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_05_04_144558_create_user_dropdowns_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDropdownsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_dropdowns', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_id");
+            $table->unsignedBigInteger("navigation_dropdowns_id");
+            $table->foreign("user_id")
+                ->references("id")
+                ->on("users")
+                ->cascadeOnDelete("cascade");
+            $table->foreign("navigation_dropdowns_id")
+                ->references("id")
+                ->on("navigation_dropdowns_id")
+                ->cascadeOnDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_dropdowns');
+    }
+}
