ba1afb2d1822fcde3c931a82b6f0d773236a4730
silvesterwali <silvesterlhwali123@gmail.com>
2021-05-07 21:10:03 +0800

    add \App\Rules\PasswordMatchRole.php and add __invoke method on \App\Http\Controllers\AuthPasswordChangeController.php

--- app/Http/Controllers/AuthPasswordChangeController.php
+++ app/Http/Controllers/AuthPasswordChangeController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Rules\PasswordMatchRole;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthPasswordChangeController extends Controller
 {
-    //
+    /**
+     * User can update their password auth
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $this->validate($request, [
+            "current_password" => ["required", new PasswordMatchRole],
+            'password'         => 'required|confirmed',
+        ]);
+
+        User::find(auth()->user()->id)->update(["password" => Hash::make($request->password)]);
+
+        return response(["message" => "Success change password"]);
+    }
 }
---
+++ app/Rules/PasswordMatchRole.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * Custom validation for match given password with current auth password
+ *
+ */
+class PasswordMatchRole implements Rule
+{
+
+    /**
+     * Determine if the validation rule passed
+     * @param string $attribute
+     * @param mixed $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return Hash::check($value, auth()->user()->password);
+    }
+
+    /**
+     * Get the validation error
+     * @return string
+     */
+    public function message()
+    {
+        return 'The :attribute is match with old password.';
+    }
+}
